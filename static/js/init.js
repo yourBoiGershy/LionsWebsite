3f60fec8ae827fae60cd58c289a87dd0e66851b4
Fred Liu <fredliu876@gmail.com>
2020-02-20 15:21:54 -0500

    Fix carousel cropping bug by using bootstrap carousel

--- static/js/init.js
+++ static/js/init.js
@@ -1,28 +1,6 @@
 var slideIndex = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-    //Carousel init
-    let carousels = document.querySelectorAll('.carousel')
-    M.Carousel.init(carousels, {
-        fullWidth:true,
-        duration:100,
-        onCycleTo: function (data) {
-            slideIndex = data.getAttribute('data-index')
-            console.log(slideIndex)
-        }
-    })
-
-    //Disable manual dragging to slide by removing event listeners for mouse actions
-    for(let i = 0; i < carousels.length; i++) {
-        let carouselInstance = M.Carousel.getInstance(carousels[i])
-
-        carousels[i].removeEventListener('mousedown', carouselInstance._handleCarouselTapBound)
-        carousels[i].removeEventListener('mousemove', carouselInstance._handleCarouselDragBound)
-        carousels[i].removeEventListener('mouseup', carouselInstance._handleCarouselReleaseBound)
-        carousels[i].removeEventListener('mouseleave', carouselInstance._handleCarouselReleaseBound)
-        carousels[i].removeEventListener('click', carouselInstance._handleCarouselClickBound)
-    }
 
     //Nav dropdown init
     let navDropdown = document.querySelectorAll('.nav-dropdown-trigger')
